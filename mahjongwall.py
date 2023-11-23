befc84d39d42378044f10f97e6e6f63ef9f06108
Conway <yawnoc@pm.me>
2023-11-24 01:53:45 +0800

    Add wind text

--- mahjongwall.py
+++ mahjongwall.py
@@ -10,23 +10,57 @@ INNER_COLUMN_COUNT = 13
 TILE_WIDTH = 28
 TILE_HEIGHT = 37
 TILE_ROUNDING_RADIUS = 5
-TILE_FILL = "green"
-TILE_BORDER = "black"
+TILE_FILL = 'green'
+TILE_BORDER = 'black'
 TILE_BORDER_WIDTH = 2
 
-ILLUSTRATION_TEMPLATE = Template(f'''\
+WIND_TEXT_FROM_INDEX = {
+    0: '東 E',
+    1: '南 S',
+    2: '西 W',
+    3: '北 N',
+}
+WIND_FONT_SIZE = '72px'
+
+ILLUSTRATION_TEMPLATE = Template('''\
 <?xml version="1.0" encoding="UTF-8"?>
 <svg viewBox="$view_left $view_top $view_width $view_height" xmlns="http://www.w3.org/2000/svg">
+<style>
+  #tile {
+    fill: $tile_fill;
+    stroke: $tile_border;
+    stroke-width: $tile_border_width;
+  }
+  .wind {
+    font: $wind_font_size sans-serif;
+  }
+  text {
+    dominant-baseline: middle;
+    text-anchor: middle;
+  }
+</style>
 <defs>
-  <rect id="tile"
-    width="$tile_width" height="$tile_height"
-    rx="$rounding_radius" ry="$rounding_radius"
-    fill="$tile_fill" stroke="$tile_border" stroke-width="$tile_border_width"
-    />
+  <rect id="tile" width="$tile_width" height="$tile_height" rx="$rounding_radius" ry="$rounding_radius" />
 </defs>
 $table_content_tilted
 </svg>
 ''')
+
+
+def build_single_wall(wall_index, column_count, inner_side_length):
+    initial_left = inner_side_length/2 - TILE_WIDTH
+    initial_top = inner_side_length/2
+    overhang_column_count = column_count - INNER_COLUMN_COUNT
+
+    return '\n'.join(
+        f'<use href="#tile" x="{initial_left - column_index * TILE_WIDTH}" y="{initial_top}" />'
+        f'<g transform="'
+        f'rotate({WALL_TILT_DEGREES}) '
+        f'translate({initial_left} {initial_top + overhang_column_count * TILE_WIDTH}) '
+        f'rotate({-wall_index * 90})'
+        f'"><text class="wind">{WIND_TEXT_FROM_INDEX[wall_index]}</text></g>'
+        for column_index in range(column_count)
+    )
 
 
 def build_svg(column_count, show_starts):
@@ -34,15 +68,10 @@ def build_svg(column_count, show_starts):
     view_width = (2 * column_count - INNER_COLUMN_COUNT) * TILE_WIDTH * (cos(wall_tilt) + sin(wall_tilt))
     inner_side_length = INNER_COLUMN_COUNT * TILE_WIDTH
 
-    east_first_tile_left = inner_side_length/2 - TILE_WIDTH
-    east_first_tile_top = inner_side_length/2
-
-    single_wall = '\n'.join(
-        f'<use href="#tile" x="{east_first_tile_left - column_index * TILE_WIDTH}" y="{east_first_tile_top}" />'
-        for column_index in range(column_count)
-    )
     four_walls = '\n'.join(
-        f'<g transform="rotate({wall_index * 90})">\n{single_wall}\n</g>'
+        f'<g transform="rotate({wall_index * 90})">\n'
+        f'{build_single_wall(wall_index, column_count, inner_side_length)}\n'
+        f'</g>'
         for wall_index in range(0, 4)
     )
 
@@ -55,6 +84,7 @@ def build_svg(column_count, show_starts):
         view_top=-view_width/2,
         view_width=view_width,
         view_height=view_width,
+        wind_font_size=WIND_FONT_SIZE,
         tile_width=TILE_WIDTH,
         tile_height=TILE_HEIGHT,
         rounding_radius=TILE_ROUNDING_RADIUS,
